dafcdaf90eb83c13a58dee0a95a0ec8fd2906e61
Daria Kolodzey <kolodaria@gmail.com>
2021-09-20 19:47:45 +0200

    Init XCode project

---
+++ CircleStrokeSpin/CircleStrokeSpin/CircleStrokeSpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleStrokeSpinApp.swift
+//  CircleStrokeSpin
+//
+//  Created by Daria Kolodzey on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct CircleStrokeSpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
